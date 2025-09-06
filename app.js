bf2b093114297693c762d40bd81c171b71690f3a
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2025-09-06 23:36:41 +0530

    Fetch todos from API

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const input = document.getElementById('todoInput');
-const button = document.getElementById('addBtn');
-const list = document.getElementById('todoList');
-
-button.addEventListener('click', () => {
-    const task = input.value.trim();
-    if (task) {
+async function fetchTodos() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+    const todos = await res.json();
+    const list = document.getElementById('todoList');
+    todos.forEach(todo => {
         const li = document.createElement('li');
-        li.textContent = task;
+        li.textContent = todo.title;
         list.appendChild(li);
-        input.value = '';
-    } 
-})
+    });
+}
+
+fetchTodos();
